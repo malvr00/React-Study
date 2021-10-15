e40282c8839643ccd4aafbc549871963cd3b067f
조용일 <yedn1128@naver.com>
2021-10-15 18:49:28 +0900

    LifeCycle

--- src/App.js
+++ src/App.js
@@ -6,9 +6,30 @@ import Counter from "./Counter";
 import EventPractice from "./EventPractice";
 import Say from "./Say";
 import { IterationSample } from "./IterationSample";
+import { LifeCycleSample } from "./LifeCycleSample";
 
-const App = () => {
-  return <IterationSample />;
-};
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>랜덩 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </>
+    );
+  }
+}
 
 export default App;
